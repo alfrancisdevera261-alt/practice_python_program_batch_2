1e23b78d5db5a24fb5abc06138b2227fc2ac9a5f
Alfrancis C. De Vera <alfrancidevera261@gmail.com>
2026-03-18 07:55:35 +0800

    added a if statement so that if the other is larger than the other number

--- print_the_between_of_2_input_numbers.py
+++ print_the_between_of_2_input_numbers.py
@@ -1,5 +1,9 @@
 number1 = int(input("Enter a number: "))
 number2 = int(input("Enter a number: "))
 
-for i in range(number1, number2-1):
-    print(i+1)
+if number1 > number2:
+    for i in range(number1, number2-1):
+        print(i+1)
+else:
+    for i in range(number2, number1-1):
+        print(i+1)
