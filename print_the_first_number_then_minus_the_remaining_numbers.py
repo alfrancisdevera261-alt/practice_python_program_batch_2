aad1b8ea89a315d9381791b9eb941b4a67debf51
Alfrancis C. De Vera <alfrancidevera261@gmail.com>
2026-03-18 07:16:50 +0800

    aaded the operators and then print the results

--- print_the_first_number_then_minus_the_remaining_numbers.py
+++ print_the_first_number_then_minus_the_remaining_numbers.py
@@ -1,3 +1,5 @@
 number1 = float(input("Enter number 1: "))
-for i in range(1, 9):
-    number2 = float(input(f"Enter number {i+2}: "))
+number2 = 0
+for i in range(0, 9):
+    number2 += float(input(f"Enter number {i+2}: "))
+print(f"The result is {number1 - number2}")
