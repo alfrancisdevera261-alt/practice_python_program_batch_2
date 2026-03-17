f2b68d7a7a5e50a4355b4e1d2cb6428b27784a28
Alfrancis C. De Vera <alfrancidevera261@gmail.com>
2026-03-18 06:26:14 +0800

    added the conditions for the 2 numbers to test which is the smaller number

--- prints_the_smaller_number_of_a_2_input_number.py
+++ prints_the_smaller_number_of_a_2_input_number.py
@@ -1,3 +1,9 @@
 number1 = int(input("Enter a number: "))
 number2 = int(input("Enter a number: "))
 
+if number1 < number2:
+    print(f"Number {number1} is smaller")
+elif number2 < number1:
+    print(f"Number {number2} is smaller")
+else:
+    print("The number are the same")
