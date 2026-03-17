5d83838f1d6cae0a85883ad30c2b6df8dc246cc1
Alfrancis C. De Vera <alfrancidevera261@gmail.com>
2026-03-18 06:32:03 +0800

    added conditions to check if the numbers are equal or not

--- checks_if_the_nubers_are_equal.py
+++ checks_if_the_nubers_are_equal.py
@@ -1,3 +1,7 @@
 number1 = int(input("Enter a number: "))
 number2 = int(input("ENter another number: "))
 
+if number1 == number2:
+    print("The numbers are equal")
+else:
+    print("The numbers are not equal")
