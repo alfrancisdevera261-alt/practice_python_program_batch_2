ef9764fd2a54291bb8824709e424c8b44a4e34a6
Alfrancis C. De Vera <alfrancidevera261@gmail.com>
2026-03-18 07:57:57 +0800

    changing the > operator to <

--- print_the_between_of_2_input_numbers.py
+++ print_the_between_of_2_input_numbers.py
@@ -1,7 +1,7 @@
 number1 = int(input("Enter a number: "))
 number2 = int(input("Enter a number: "))
 
-if number1 > number2:
+if number1 < number2:
     for i in range(number1, number2-1):
         print(i+1)
 else:
